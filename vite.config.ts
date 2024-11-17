4b24fcd30278081886b22e3e01d20fb578c963c6
ronniepeet-sage <ronnie.peet@sage.com>
2024-11-17 23:58:36 +0000

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    hmr: {
+      overlay: true
+    }
+  },
+  optimizeDeps: {
+    include: ['zustand', 'react-dropzone', 'marked', 'd3']
+  },
+  css: {
+    postcss: './postcss.config.js'
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+});
